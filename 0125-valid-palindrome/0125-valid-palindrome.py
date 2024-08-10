c007b3ddd1afe23db000086689ef2d243687854b
DEEPAK PANDEY <140734262+848deepak@users.noreply.github.com>
2024-08-10 23:35:28 +0530

    Time: 21 ms (91.21%), Space: 14.2 MB (16.24%) - LeetHub

---
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -0,0 +1,15 @@
+
+
+class Solution(object):
+    def isPalindrome(self, s):
+        
+       
+        s = s.lower()
+        
+        
+        s = re.sub(r'[^a-z0-9]', '', s)
+        
+        
+        return s == s[::-1]
+
+
